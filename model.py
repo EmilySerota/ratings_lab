b51a86dca8fe86acfb23d1e634b8d2ea7fb0dea9
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-30 12:52:19 -0700

    finished day 1 of ratings lab 'judgemental eye'

--- model.py
+++ model.py
@@ -36,7 +36,7 @@ class Movie(db.Model):
 
     movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    released_at = db.Column(db.DateTime, nullable=False)
+    released_at = db.Column(db.DateTime, nullable=True)
     imdb_url = db.Column(db.String(400), nullable=False)
 
 class Rating(db.Model):
