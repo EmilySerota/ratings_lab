020ae9c53e312b215a7d94d33781f6d8d8dd43d4
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-31 17:41:52 -0700

    updated to user list, redirect broken

--- server.py
+++ server.py
@@ -24,6 +24,7 @@ def index():
     """Homepage."""
     return render_template('homepage.html')
 
+
 @app.route('/users')
 def user_list():
     """show list of users"""
@@ -31,11 +32,19 @@ def user_list():
     users = User.query.all()
     return render_template('user_list.html', users=users)
 
+
+@app.route('/users/<user_id>')
+def user_detail(user_id):
+
+    user = User.query.get(user_id)
+    return render_template('user_info.html', user=user)
+
 @app.route('/register', methods=['GET'])
 def register_form():
     """show registration form"""
 
     return render_template('register_form.html')
+
 
 @app.route('/register', methods=['POST'])
 def register_process():
@@ -44,11 +53,13 @@ def register_process():
     email = request.form.get('email')
     password = request.form.get('password')
 
-    if email not in User.query.filter_by(email == email):
+    # if email not in db, add them to db
+
+    if not User.query.filter_by(email = email).all():
+        print(User.query.filter_by(email = email).all())
         new_user = User(email=email, password=password)
         db.session.add(new_user)
         db.session.commit()
-        
 
     return redirect('/')
 
@@ -58,6 +69,40 @@ def login_form():
     """show login form"""
 
     return render_template('login_form.html')
+
+
+@app.route('/login', methods=['POST'])
+def login_process():
+    """process login form"""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+
+    # If email not in db, bring them to register page
+    if not User.query.filter_by(email = email).all():
+        return redirect('/register')
+
+    # If password doesn't match email, alert them and bring back to login page
+    if not User.query.filter_by(email = email, password = password).all():
+        flash("Incorrect password")
+        return redirect('/login')
+
+    else:
+        User.query.filter_by(user_id = user).all()
+    # otherwise if successful login, redirect to homepage - changing to render user page
+    session['username'] = email
+    #flash("Logged In")
+    #users = User.query.all()
+    return redirect(f"/users/{user.user_id}")
+
+
+@app.route('/logout')
+def logout():
+    """process logout"""
+    session.pop('username',None)
+    flash("You are now logged out")
+    return redirect('/')
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
@@ -71,4 +116,4 @@ if __name__ == "__main__":
     # Use the DebugToolbar
     DebugToolbarExtension(app)
 
-    app.run(port=5000, host='0.0.0.0')
+    app.run()
